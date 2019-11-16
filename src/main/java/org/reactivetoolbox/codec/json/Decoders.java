d5c3039559159cb99bacf3717d08cbf209716653
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2019-11-16 18:14:14 +0100

    Add decoding for integer values

--- src/main/java/org/reactivetoolbox/codec/json/Decoders.java
+++ src/main/java/org/reactivetoolbox/codec/json/Decoders.java
@@ -1,5 +1,6 @@
 package org.reactivetoolbox.codec.json;
 
+import org.reactivetoolbox.core.lang.Functions.FN1;
 import org.reactivetoolbox.core.lang.Option;
 import org.reactivetoolbox.core.lang.Result;
 import org.reactivetoolbox.core.lang.support.KSUID;
@@ -29,21 +30,25 @@ public interface Decoders {
         return answer(null);
     }
 
-    static Result<Option<Boolean>> bool(final Token input) {
-        return expect(input, LITERAL)
-                .flatMap(Decoders::boolDecoder);
-    }
-
     static <T> Result<Option<T>> nullDecoder(String literal) {
         return "null".equals(literal) ? answerEmpty()
                                       : error("Unrecognized literal {0}", literal);
     }
 
+    static <T> Result<Option<T>> decodeNull(final Token input) {
+        return expect(input, LITERAL).flatMap(Decoders::nullDecoder);
+    }
+
+    static Result<Option<Boolean>> bool(final Token input) {
+        return expect(input, LITERAL)
+                .flatMap(Decoders::boolDecoder);
+    }
+
     static Result<Option<Boolean>> boolDecoder(String literal) {
-        return "null".equals(literal) ? answerEmpty()
-                                      : "true".equals(literal) ? answer(true)
-                                                               : "false".equals(literal) ? answer(false)
-                                                                                         : error("Unknown value for boolean literal {0}", literal);
+        return "null".equals(literal) ? answerEmpty() :
+               "true".equals(literal) ? answer(true) :
+               "false".equals(literal) ? answer(false) :
+               error("Unknown value for boolean literal {0}", literal);
     }
 
     static Result<Option<String>> string(final Token input) {
@@ -51,24 +56,45 @@ public interface Decoders {
                                     .or(() -> decodeNull(input));
     }
 
-    static Result<Option<String>> decodeNull(final Token input) {
-        return expect(input, LITERAL).flatMap(Decoders::nullDecoder);
-    }
-
     static Result<Option<Byte>> byteInt(final Token input) {
-        return error("NOT IMPLEMENTED");
+        return expect(input, INTEGER).flatMap(string -> integerDecoder(string, Byte.MIN_VALUE, Byte.MAX_VALUE, Long::byteValue))
+                                     .or(() -> decodeNull(input));
     }
 
     static Result<Option<Short>> shortInt(final Token input) {
-        return error("NOT IMPLEMENTED");
-    }
-
-    static Result<Option<Long>> longInt(final Token input) {
-        return error("NOT IMPLEMENTED");
+        return expect(input, INTEGER).flatMap(string -> integerDecoder(string, Short.MIN_VALUE, Short.MAX_VALUE, Long::shortValue))
+                                     .or(() -> decodeNull(input));
     }
 
     static Result<Option<Integer>> regularInt(final Token input) {
-        return error("NOT IMPLEMENTED");
+        return expect(input, INTEGER).flatMap(string -> integerDecoder(string, Integer.MIN_VALUE, Integer.MAX_VALUE, Long::intValue))
+                                     .or(() -> decodeNull(input));
+    }
+
+    static Result<Option<Long>> longInt(final Token input) {
+        return expect(input, INTEGER).flatMap(string -> integerDecoder(string, Long.MIN_VALUE, Long.MAX_VALUE, Long::longValue))
+                                     .or(() -> decodeNull(input));
+    }
+
+    static <T extends Number> Result<Option<T>> integerDecoder(final String string, final long minValue, final long maxValue, final FN1<T, Long> mapper) {
+        return longDecoder(string).flatMap(v -> boundsChecker(v, minValue, maxValue))
+                                  .map(o -> o.map(mapper));
+    }
+
+    static Result<Option<Long>> boundsChecker(final Option<Long> value, final long minValue, final long maxValue) {
+        return value.map($ -> Result.success(value),
+                         val -> (val >= minValue && val <= maxValue) ? answer(val)
+                                                                     : error("Value is out of bounds of the given type, value={0}, min={1}, max={2}",
+                                                                             value, minValue, maxValue));
+
+    }
+
+    static Result<Option<Long>> longDecoder(final String string) {
+        try {
+            return answer(Long.parseLong(string));
+        } catch (final NumberFormatException e) {
+            return error("Invalid integer value {0}", string);
+        }
     }
 
     static Result<Option<Float>> floatNumber(final Token input) {
--- src/test/java/org/reactivetoolbox/codec/json/DecoderTest.java
+++ src/test/java/org/reactivetoolbox/codec/json/DecoderTest.java
@@ -1,6 +1,7 @@
 package org.reactivetoolbox.codec.json;
 
 import org.junit.jupiter.api.Test;
+import org.reactivetoolbox.core.lang.Option;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -41,6 +42,74 @@ class DecoderTest {
                           .onSuccess(v -> assertEquals(option(""), v))
                           .onFailure(f -> fail());
         decoder("null").read(String.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+    }
+
+    @Test
+    void canReadBytePrimitives() {
+        decoder("3").read(byte.class)
+                    .onSuccess(v -> assertEquals(option((byte) 3), v))
+                    .onFailure(f -> fail());
+        decoder("null").read(byte.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+
+        decoder("3").read(Byte.class)
+                    .onSuccess(v -> assertEquals(option((byte) 3), v))
+                    .onFailure(f -> fail());
+        decoder("null").read(Byte.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+    }
+
+    @Test
+    void canReadShortPrimitives() {
+        decoder("456").read(short.class)
+                      .onSuccess(v -> assertEquals(option((short) 456), v))
+                      .onFailure(f -> fail());
+        decoder("null").read(short.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+
+        decoder("789").read(Short.class)
+                      .onSuccess(v -> assertEquals(option((short) 789), v))
+                      .onFailure(f -> fail());
+        decoder("null").read(Short.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+    }
+
+    @Test
+    void canReadIntPrimitives() {
+        decoder("4563456").read(int.class)
+                      .onSuccess(v -> assertEquals(option(4563456), v))
+                      .onFailure(f -> fail());
+        decoder("null").read(int.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+
+        decoder("7892343").read(Integer.class)
+                      .onSuccess(v -> assertEquals(option(7892343), v))
+                      .onFailure(f -> fail());
+        decoder("null").read(Integer.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+    }
+
+    @Test
+    void canReadLongPrimitives() {
+        decoder("1234563456").read(long.class)
+                          .onSuccess(v -> assertEquals(option(1234563456L), v))
+                          .onFailure(f -> fail());
+        decoder("null").read(long.class)
+                       .onSuccess(v -> assertEquals(empty(), v))
+                       .onFailure(f -> fail());
+
+        decoder("1237892343").read(Long.class)
+                          .onSuccess(v -> assertEquals(option(1237892343L), v))
+                          .onFailure(f -> fail());
+        decoder("null").read(Long.class)
                        .onSuccess(v -> assertEquals(empty(), v))
                        .onFailure(f -> fail());
     }
